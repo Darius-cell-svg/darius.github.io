2d1ae2631e91423937d7f0a2c5a1faa3cf51cf7a
Darius-cell-svg <gamerprovova1@gmail.com>
2025-11-10 11:17:55 +1200

    Булхов 25.11 ИСв

---
+++ main.js
@@ -0,0 +1,26 @@
+function setSize(size) {
+    
+    document.querySelectorAll('.size-btn').forEach(btn => {
+        btn.classList.remove('active');
+    });
+    
+    
+    event.target.classList.add('active');
+    
+    
+    document.body.classList.remove('desktop-size', 'tablet-size', 'mobile-size');
+    
+    
+    if (size === 'tablet') {
+        document.body.classList.add('tablet-size');
+    } else if (size === 'mobile') {
+        document.body.classList.add('mobile-size');
+    } else {
+        document.body.classList.add('desktop-size');
+    }
+}
+
+
+document.querySelector('.burger-menu').addEventListener('click', function() {
+    alert('Меню открыто!');
+});
